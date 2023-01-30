75388f6bb4dc35d2479f3752546b0e7d125b0c97
ronlip2571 <sanchezronphilip@gmail.com>
2023-01-30 21:52:09 +0800

    first commit

--- components/About.jsx
+++ components/About.jsx
@@ -12,17 +12,17 @@ const About = () => {
             About
           </p>
           <h2 className='py-4'>Who I Am</h2>
-          <p className='py-2  text-black'>
+          <p className='py-2  text-black font-semibold hover:scale-125 ease-in duration-300'>
             I specialize in building mobile responsive front-end UI applications
             that connect with API and other backend technologies.</p>
-            <p className='py-2 text-black'> I am passionate about learning new technologies and understand there is more than one way to accomplish a task.</p>
-            <p className='py-2 text-black'>Though I am most proficient in building front-end applications using HTML, CSS, Javascript, and React.</p> 
-            <p className='py-2 text-black'>I am a quick learner and can pick up new tech stacks as needed.</p>
-            <p className='py-2 text-black'>
+            <p className='py-2 text-black font-semibold hover:scale-125 ease-in duration-300'> I am passionate about learning new technologies and understand there is more than one way to accomplish a task.</p>
+            <p className='py-2 text-black font-semibold hover:scale-125 ease-in duration-300'>Though I am most proficient in building front-end applications using <span className=' font-extrabold'>HTML, CSS, Javascript, and React.</span></p> 
+            <p className='py-2 text-black font-semibold hover:scale-125 ease-in duration-300'>I am a quick learner and can pick up new tech stacks as needed.</p>
+            <p className='py-2 text-black font-semibold hover:scale-125 ease-in duration-300'>
             I believe that being a great developer is not using one
             specific language, but choosing the best tool for the job.</p>
           <Link href='/#projects'>
-            <p className='py-2 text-gray-600 underline cursor-pointer'>
+            <p className='py-2 text-black underline cursor-pointer font-semibold hover:scale-125 ease-in duration-300'>
               Check out some of my latest projects.
             </p>
           </Link>
