a505b6183215a3b898d59b084d7a4397839a9a63
ronlip2571 <sanchezronphilip@gmail.com>
2023-01-30 20:19:58 +0800

    first commit

--- components/About.jsx
+++ components/About.jsx
@@ -12,17 +12,15 @@ const About = () => {
             About
           </p>
           <h2 className='py-4'>Who I Am</h2>
-          <p className='py-2 text-gray-600'>I specialize in building mobile responsive front-end UI applications
-            that connect with API and other backend technologies. I am
-            passionate about learning new technologies and understand there is
-            more than one way to accomplish a task. Though I am most proficient
-            in building front-end applications using HTML, CSS, Javascript, and
-            React, I am a quick learner and can pick up new tech stacks as
-            needed. I believe that being a great developer is not using one
-            specific language, but choosing the best tool for the job.</p>
           <p className='py-2 text-gray-600'>
-           {/*  */}
-          </p>
+            I specialize in building mobile responsive front-end UI applications
+            that connect with API and other backend technologies.</p>
+            <p className='py-2 text-gray-600'> I am passionate about learning new technologies and understand there is more than one way to accomplish a task.</p>
+            <p className='py-2 text-gray-600'>Though I am most proficient in building front-end applications using HTML, CSS, Javascript, and React.</p> 
+            <p className='py-2 text-gray-600'>I am a quick learner and can pick up new tech stacks as needed.</p>
+            <p className='py-2 text-gray-600'>
+            I believe that being a great developer is not using one
+            specific language, but choosing the best tool for the job.</p>
           <Link href='/#projects'>
             <p className='py-2 text-gray-600 underline cursor-pointer'>
               Check out some of my latest projects.
