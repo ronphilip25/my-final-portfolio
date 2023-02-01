7b0484dd58f068a9772c5519b7462f9f7ed9f604
ronlip2571 <sanchezronphilip@gmail.com>
2023-02-01 23:27:00 +0800

    14th commit

--- components/ProjectItems.jsx
+++ components/ProjectItems.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Link from 'next/link'
 import Image from 'next/image'
-import PetImg from '../public/petcommerce.JPG'
+import PetImg from '../public/petcommerce.jpg'
 import Soon from '../public/soon.jpg'
 
 function ProjectItems() {
