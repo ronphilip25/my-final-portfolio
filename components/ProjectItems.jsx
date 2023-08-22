4d6723a4c649e4eaa2a55cfd823a43d375fa55ec
ronlip2571 <sanchezronphilip@gmail.com>
2023-08-22 12:01:33 +0800

    last update

--- components/ProjectItems.jsx
+++ components/ProjectItems.jsx
@@ -24,12 +24,12 @@ function ProjectItems() {
           </div>
           <div className='relative flex items-center justify-items-center h-auto w-full shadow-xl shadow-slate-800 rounded-xl p-4 group hover:bg-gradient-to-r from-[#164e63] to-[#22d3ee]'>
               <div className='hidden group-hover:block absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]'>
-                 <h3 className='text-2xl text-white tracking-wider text-center'>??</h3>
-                 <p className='pb-4 pt-2 text-white text-center'>E-Commerce/Coop</p>
+                 <h3 className='text-2xl text-white tracking-wider text-center'>E-Commerce/COOP</h3>
+                 <p className='pb-4 pt-2 text-white text-center'>Reactjs/Redux</p>
                  <p className='text-center py-3 rounded-lg bg-white text-gray-700 font-bold text-lg cursor-pointer'>Click Here</p>
               </div>
               <Link href='https://sample-e-commerce-three.vercel.app/products'>
-              <Image className='rounded-xl group-hover:opacity-10 '
+              <Image className='rounded-xl group-hover:opacity-10 h-full'
                 src={coopImage}
                 alt='Logo'
               />
