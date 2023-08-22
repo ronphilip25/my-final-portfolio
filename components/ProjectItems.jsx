c0e88c53c76f78583841e73fbfc7f040272ae4f5
ronlip2571 <sanchezronphilip@gmail.com>
2023-08-22 11:43:39 +0800

    updatedv12

--- components/ProjectItems.jsx
+++ components/ProjectItems.jsx
@@ -3,7 +3,7 @@ import Link from 'next/link'
 import Image from 'next/image'
 import PetImg from '../public/petcommerce.jpg'
 import Soon from '../public/soon.jpg'
-import coop from '../public/coop'
+import Coop from '../public/coop'
 
 function ProjectItems() {
   return (
@@ -24,12 +24,12 @@ function ProjectItems() {
           <div className='relative flex items-center justify-items-center h-auto w-full shadow-xl shadow-slate-800 rounded-xl p-4 group hover:bg-gradient-to-r from-[#164e63] to-[#22d3ee]'>
               <div className='hidden group-hover:block absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]'>
                  <h3 className='text-2xl text-white tracking-wider text-center'>??</h3>
-                 <p className='pb-4 pt-2 text-white text-center'>NextJs, ReactJs</p>
+                 <p className='pb-4 pt-2 text-white text-center'>E-Commerce/Coop</p>
                  <p className='text-center py-3 rounded-lg bg-white text-gray-700 font-bold text-lg cursor-pointer'>Click Here</p>
               </div>
               <Link href='https://sample-e-commerce-three.vercel.app/products'>
               <Image className='rounded-xl group-hover:opacity-10 '
-                src={coop}
+                src={Coop}
                 alt='Logo'
               />
             </Link>
